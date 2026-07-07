041420f4ba0dd945afda4b8b947291e3888ff1bf
anbesh-ai <nirmala.chapagai@icloud.com>
2026-07-07 15:10:16 +0545

    Initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, redirect, render_template, url_for
+from flask import request
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
+#from app import app, db
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///userr.db"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(70), nullable=False)
+    password = db.Column(db.String(70), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    datetime = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()         
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/welcome')
+def welcome_page():
+    return render_template("welcome.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/visit')
+def visit():
+    return render_template("visit.html")
+
+@app.route('/registration', methods=["GET", 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        description = request.form.get('description')
+        print(f"Username: {username}, Password: {password}, Description: {description}")
+        new_user = User(username=username, password=password, description=description)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('register.html')
+
+
+if __name__ == "__main__":
+    app.run(port=5000)     
